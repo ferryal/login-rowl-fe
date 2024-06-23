3959e385ade39382eb7f0136c4981ae2e0c6a33e
Ferryal Fajar <ferryal21@gmail.com>
2024-06-23 10:38:08 +0700

    add redirect & env

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    JWT_SECRET: process.env.JWT_SECRET,
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/login",
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
